ec2f7aee2000858805bce28873f5f28787d0cd78
Leo Morton <leohmorton@gmail.com>
2024-12-12 21:38:33 +0000

    added sky

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,18 @@
 "use client";
 // import ActionButton from "./components/ActionButton";
 import { useEffect, useState } from "react";
+import { set } from "zod";
 
 export default function HomePage() {
-  const [currentSparkle, setCurrentSparkle] = useState("");
+  const [skySparkle, setSkySparkle] = useState("");
+  const [waterSparkle, setWaterSparkle] = useState("");
 
   useEffect(() => {
+    setSkySparkle(makeMultipleLines(12, 200, true, 0.01));
+    setWaterSparkle(makeMultipleLines(12, 50, false));
     const interval = setInterval(() => {
-      setCurrentSparkle(makeMultipleLines(24));
+      // setSkySparkle(makeMultipleLines(14, 100, true, 0.05));
+      setWaterSparkle(makeMultipleLines(12, 50, false));
     }, 2000);
     return () => clearInterval(interval);
   }, []);
@@ -15,28 +20,41 @@ export default function HomePage() {
     character: string,
     width: number,
     probability: number,
+    evenSpacing = true,
   ): string => {
     return Array.from({ length: width }, (x, i) => {
-      // calculate distance from center
-      const distance = Math.abs(i - width / 2);
-      // adjust probability based on exponential distance from center
-      const adjustedProbability =
-        probability * Math.exp(-distance / (width / 15));
+      let adjustedProbability = probability;
+      if (!evenSpacing) {
+        // calculate distance from center
+        const distance = Math.abs(i - width / 2);
+        // adjust probability based on exponential distance from center
+        adjustedProbability = probability * Math.exp(-distance / (width / 10));
+      }
       return Math.random() < adjustedProbability ? character : " ";
     }).join("");
   };
-  const makeMultipleLines = (count: number) => {
-    return Array.from({ length: count }, (x, i) => {
-      return makeSparkleLine(".", 100, 0.6 - i * 0.02);
+  const makeMultipleLines = (
+    height: number,
+    width: number,
+    evenSpacing = true,
+    probability = 0.1,
+  ) => {
+    return Array.from({ length: height }, (x, i) => {
+      i -= 0.001;
+      let adjustedProbability = probability;
+      if (!evenSpacing) {
+        adjustedProbability = 3 / i;
+      }
+      return makeSparkleLine(".", width, adjustedProbability, evenSpacing);
     }).join("\n");
   };
   return (
     <main className="flex min-h-screen flex-col items-center gap-4 overflow-hidden bg-black p-4 text-white">
       <p className="font-mono text-sm">a peaceful night on the water...</p>
-      <br />
-      <br />
+      <pre>{skySparkle}</pre>
       <p>O</p>
-      <pre>{currentSparkle}</pre>
+      {/* <pre>{makeSparkleLine(".", 100, 5)}</pre> */}
+      <pre>{waterSparkle}</pre>
     </main>
   );
 }
